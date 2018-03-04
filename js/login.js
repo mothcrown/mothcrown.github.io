b2f77de534bc8f6a84c5a2b9569bf17f8e2c8a35
Marcos <uchodc@gmail.com>
2018-03-04 17:39:00 +0000

    Testeando login Google 1

--- js/login.js
+++ js/login.js
@@ -10,6 +10,26 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 /* global React $ */
 
+function onSignInGoogle(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+  $('.g-signin2').css('display', 'none');
+  $('.data').css('display', 'block');
+  $('#pic').attr('src', profile.getImageUrl());
+  $('#email').text(profile.getEmail());
+}
+
+function signOutGoogle() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    $('.g-signin2').css('display', 'block');
+    $('.data').css('display', 'none');
+  });
+}
+
 var Registro = function (_React$Component) {
   _inherits(Registro, _React$Component);
 
