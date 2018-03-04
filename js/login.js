9b0f9a1a6166c1b53804e2f35c679bf6b679ea5a
Marcos <uchodc@gmail.com>
2018-03-04 18:05:55 +0000

    Testing google 3

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ function onSignInGoogle(googleUser) {
   console.log('Email: ' + profile.getEmail());
   // $('.g-signin2').css('display', 'none');
   $('#login').empty();
-  $('#login').append('.data');
+  $('#login').append($('.data'));
   $('.data').css('display', 'block');
   $('#pic').attr('src', profile.getImageUrl());
   $('#email').text(profile.getEmail());
